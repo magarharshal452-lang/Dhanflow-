8e0ff3f53ac68e26a9fbc5b1ce1925101bccdd92
magarharshal452-lang <magarharshal452@gmail.com>
2026-01-21 00:12:50 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Dhanflow-financial-tracker-app-2.0/',
+})
